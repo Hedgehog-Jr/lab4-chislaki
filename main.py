14a127f57973ebc626db1cf30b52516b9df26f4c
Pavel Ozhich <pavel2.ozhich@gmail.com>
2019-10-10 17:20:49 +0300

    fix

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def main():
     xx = [0.164, 0.328, 0.656, 0.984, 1.312, 1.640]
     x = [0] * N
     for i in range(N):
-        x[i] = 1 / xx[i];
+        x[i] = 1 / xx[i]
     y = [0.448, 0.432, 0.421, 0.417, 0.414, 0.412]
-    POWERX = [0] * (2 * m);
+    POWERX = [0] * (2 * m)
     for k in range(2 * m):
         for i in range(N):
             POWERX[k] += pow(x[i], k + 1)
@@ -26,11 +26,11 @@ def main():
             PRAW[l] += y[i] * pow(x[i], l)
     a = gauss(SUMX, PRAW, m + 1)
     print("\nКоэффициенты a = ", a)
-    S2 = 1 / (N - m - 1)
+    S2 = 0
     for i in range(N):
         Sum = y[i]
         for j in range(m + 1):
-            Sum -= a[m] * pow(x[i], m)
+            Sum -= a[j] * pow(x[i], j)
         S2 += pow(Sum, 2)
     S2 /= N - m - 1
     sigma = pow(S2, 0.5)
